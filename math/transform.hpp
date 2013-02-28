c76e189d4b80bb841559a6b0a13205b8a9e5c910
Jakub Červený <jakub.cerveny@citationtech.net>
2013-02-28 09:20:51 +0000

    math/transform: fixed Extents2 transform
    git-svn-id: https://scm.citationtech.net/trunk/shared/math/math@3398 b7e6147c-0f76-4c84-a09d-19069afaf10c

--- math/transform.hpp
+++ math/transform.hpp
@@ -28,6 +28,15 @@ inline Point2 transform(const Matrix4& tr, const Point2 &pt)
         tr(1,0)*pt(0) + tr(1,1)*pt(1) + tr(1,3));
 }
 
+inline Extents2 transform(const Matrix4& tr, const Extents2& extents)
+{
+    return Extents2(
+        transform(tr, extents.ll),
+        transform(tr, extents.ur));
+}
+
+// in-place transformations for vectors:
+
 inline void transform(const Matrix4& tr, Points3& points)
 {
     for (Point3& pt : points)
@@ -38,12 +47,6 @@ inline void transform(const Matrix4& tr, Points2& points)
 {
     for (Point2& pt : points)
         pt = transform(tr, pt);
-}
-
-inline void transform(const Matrix4& tr, Extents2& extents)
-{
-    transform(tr, extents.ll);
-    transform(tr, extents.ur);
 }
 
 } // namespace math
