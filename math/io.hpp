7db0ef8eefb68f6c4205803c7487fa2dbb240905
Václav Blažek <vaclav.blazek@citationtech.net>
2015-03-20 15:17:01 +0000

    shared/math: fixed math/io.hpp
    git-svn-id: https://scm.citationtech.net/trunk/shared/math/math@12862 b7e6147c-0f76-4c84-a09d-19069afaf10c

--- math/io.hpp
+++ math/io.hpp
@@ -7,32 +7,99 @@
 
 #include "utility/streams.hpp"
 
+#include "dbglog/dbglog.hpp"
+
 #include "./geometry_core.hpp"
 
 namespace math {
 
-/** IO wrapper around Point2.
+/** IO wrappers around Point2.
+ */
+
+/** Non-const Point2_ wrapper.
+ *
+ *  Input/Output format: %d,%d
+ *
+ *  If there is no bound point all changes are made on local member.
+ *
+ *  Assignment to wrapper changes:
+ *      * bound point if wrapper is bound to valid point
+ *      * local point if wrapper is not bound
  */
 template <typename T> struct Point2IOWrapper;
 
-/** Convenient generator for Point2IOWrapper.
+/** Const Point2_ wrapper.
+ *
+ *  Input/Output format: %d,%d
+ *
+ *  Allows only output.
  */
-template <typename T> Point2IOWrapper<T> point2IOWrapper(math::Point2_<T> &p);
+template <typename T> struct ConstPoint2IOWrapper;
+
+/** Convenient generator for Point2IOWrapper.
+ *  Returns proper type of Point2IOWrapper based on type of Point2_
+ *  Binds wrapper to this point.
+ */
+template <typename T>
+Point2IOWrapper<T> point2IOWrapper(Point2_<T> &p);
+
+/** Convenient generator for ConstPoint2IOWrapper.
+ *  Returns proper type of Point2IOWrapper based on type of Point2_
+ */
+template <typename T>
+ConstPoint2IOWrapper<T> point2IOWrapper(const Point2_<T> &p);
 
 // definitions
 
 template <typename T>
 struct Point2IOWrapper {
-    Point2IOWrapper(math::Point2_<T> &p) : p(&p) {}
-    math::Point2_<T> *p;
+    Point2IOWrapper() : p_() {}
+    Point2IOWrapper(Point2_<T> &p) : p_(&p) {}
+    Point2IOWrapper(const Point2IOWrapper &other);
+    Point2IOWrapper& operator=(const Point2IOWrapper &other);
+
+    Point2_<T> &get() const;
+
+private:
+    Point2_<T> *p_;
+    mutable Point2_<T> self_;
 };
+
+template <typename T>
+struct ConstPoint2IOWrapper {
+    ConstPoint2IOWrapper(const Point2_<T> &p) : p_(&p) {}
+    const Point2_<T> &get() const { return *p_; }
+
+private:
+    const Point2_<T> *p_;
+};
+
+template <typename T>
+inline Point2IOWrapper<T>::Point2IOWrapper(const Point2IOWrapper &other)
+    : p_(other.p_), self_(other.self_) {}
+
+template <typename T>
+inline Point2IOWrapper<T>&
+Point2IOWrapper<T>::operator=(const Point2IOWrapper &other) {
+    if (&other != this) {
+        if (p_) {
+            *p_ = other.get();
+        } else {
+            self_ = other.get();
+        }
+    }
+    return *this;
+}
+
+template <typename T>
+Point2_<T>& Point2IOWrapper<T>::get() const { return p_ ? *p_ : self_; }
 
 template<typename CharT, typename Traits, typename T>
 inline std::basic_istream<CharT, Traits>&
 operator>>(std::basic_istream<CharT, Traits> &is
            , const Point2IOWrapper<T> &r)
 {
-    return is >> (*r.p)(0) >> utility::expect(',') >> (*r.p)(1);
+    return is >> r.get()(0) >> utility::expect(',') >> r.get()(1);
 }
 
 template<typename CharT, typename Traits, typename T>
@@ -40,13 +107,27 @@ inline std::basic_ostream<CharT, Traits>&
 operator<<(std::basic_ostream<CharT, Traits> &os
            , const Point2IOWrapper<T> &r)
 {
-    return os << (*r.p)(0) << ',' << (*r.p)(1);
+    return os << r.get()(0) << ',' << r.get()(1);
+}
+
+template<typename CharT, typename Traits, typename T>
+inline std::basic_ostream<CharT, Traits>&
+operator<<(std::basic_ostream<CharT, Traits> &os
+           , const ConstPoint2IOWrapper<T> &r)
+{
+    return os << r.get()(0) << ',' << r.get()(1);
 }
 
 template <typename T>
-inline Point2IOWrapper<T> point2IOWrapper(math::Point2_<T> &p)
+inline Point2IOWrapper<T> point2IOWrapper(Point2_<T> &p)
 {
     return Point2IOWrapper<T>(p);
+}
+
+template <typename T>
+inline ConstPoint2IOWrapper<T> point2IOWrapper(const Point2_<T> &p)
+{
+    return ConstPoint2IOWrapper<T>(p);
 }
 
 } // namespace math
