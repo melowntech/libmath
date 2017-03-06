4d13236ec55171f5b23a29f4ecff143b5ea14184
Vaclav Blazek <vaclav.blazek@citationtech.net>
2017-03-06 20:53:52 +0100

    transform: added support for Matrix3

--- math/transform.hpp
+++ math/transform.hpp
@@ -13,6 +13,8 @@
 
 namespace math {
 
+// matrix4
+
 inline Point3 transform(const Matrix4& tr, const Point3 &pt)
 {
     return Point3(
@@ -63,6 +65,50 @@ inline void transform(const Matrix4& tr, std::vector<cv::Point3_<T> >& points)
 {
     for (cv::Point3_<T>& pt : points)
         pt = transform(tr, pt);
+}
+
+
+// matrix3
+
+inline Point3 transform(const Matrix3 &tr, const Point2 &pt)
+{
+    return Point3(
+        tr(0,0)*pt(0) + tr(0,1)*pt(1) + tr(0,2),
+        tr(1,0)*pt(0) + tr(1,1)*pt(1) + tr(1,2),
+        tr(2,0)*pt(0) + tr(2,1)*pt(1) + tr(2,2));
+}
+
+inline Extents2 transform(const Matrix3 &tr, const Extents2 &extents)
+{
+    return Extents2(
+        transform(tr, extents.ll),
+        transform(tr, extents.ur));
+}
+
+template<typename T>
+inline cv::Point3_<T> transform(const Matrix3 &tr, const cv::Point_<T> &pt)
+{
+    return cv::Point3_<T>(
+        tr(0,0)*pt.x + tr(0,1)*pt.y + tr(0,2),
+        tr(1,0)*pt.x + tr(1,1)*pt.y + tr(1,2),
+        tr(2,0)*pt.x + tr(2,1)*pt.y + tr(2,2));
+}
+
+// in-place transformations for vectors:
+
+inline void transform(const Matrix3 &tr, Points2 &points)
+{
+    for (auto &pt : points) {
+        pt = transform(tr, pt);
+    }
+}
+
+template<typename T>
+inline void transform(const Matrix3 &tr, std::vector<cv::Point_<T> > &points)
+{
+    for (auto &pt : points) {
+        pt = transform(tr, pt);
+    }
 }
 
 } // namespace math
